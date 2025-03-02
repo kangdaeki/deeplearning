bbb3f3a6bfe40527f034bb21f024ec0eb19225a9
kangdaeki <dkkang@gmail.com>
2025-03-03 02:20:26 +0900

    sigmoid

--- Lab102.py
+++ Lab102.py
@@ -1,1 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+def sigmoid(x):
+    s=1/(1+np.exp(-x))
+    ds=s*(1-s)
+    return s,ds
+
+X=np.linspace(-10, 10, 100)
+Y1,Y2=sigmoid(X)
+
+plt.plot(X,Y1,X,Y2)
+plt.xlabel("x")
+plt.ylabel("Sigmoid(X), Sigmoid'(X)")
+plt.show()
